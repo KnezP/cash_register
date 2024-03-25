d2b7c7743722f3ab11bae1676bc42e12350c0bd1
KnezP <petarknezovic08@gmail.com>
2024-03-25 21:48:22 +0100

    adjusting output

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ let cid =[
     ["FIVE", 0],
     ["TEN", 0],
     ["TWENTY", 0],
-    ["ONE HUNDRED", 0]]
+    ["ONE HUNDRED", 0]];
 
 const values = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
 const price = 19.5;
 cid.reverse().forEach((item) => { changeAmount.innerHTML += `<p id="${item[0]}" class="changes" >${item[0]}: $${item[1]}
-</p>`})
+</p>`});
 const checkStatus = () => {
     let money = parseFloat(customerCash.value);
     let amount = parseFloat(cid.reduce((total, item) => total + parseFloat(item[1]), 0).toFixed(2));
@@ -28,19 +28,20 @@ const checkStatus = () => {
         result.innerText = "No change due - customer paid with exact cash";
     }
     else if(checkSufficient(money) === 0){
-        result.innerText = "Status: INSUFFICIENT_FUNDS"
+        result.innerText = "Status: INSUFFICIENT_FUNDS\u00A0";
     }
     else if ((amount - ((money-price).toFixed(2))) === 0 || amount === 0) {
-        result.innerText = "Status: CLOSED\n";
+        result.innerText = "Status: CLOSED\u00A0";
         let change = (money - price).toFixed(2);
         let startPoint = startingValue(change);
         calcChange(startPoint, change);
     } else {
-        result.innerText = "Status: OPEN\n";
+        result.innerText = "Status: OPEN\u00A0";
         let change = (money - price).toFixed(2);
         let startPoint = startingValue(change);
         calcChange(startPoint, change);
-    } 
+    }
+    console.log(result.innerText)
 };
 const calcChangeAvailable = (amountAvailable) => {
     return amountAvailable.reduce((total, item) => total + parseFloat(item[1]), 0);
@@ -60,7 +61,7 @@ const calcChange = (start, change) => {
             counter++;
         }
         if (change >= 0 && counter > 0) {
-            result.innerText += `${ci[i][0]}: $${counter * values[i]}\n`;
+            result.innerText += `${ci[i][0]}:\u00A0$${counter * values[i]}\n`;
             ci[i][1] = (ci[i][1] - counter * values[i]).toFixed(2);
             update(ci);
         }
@@ -78,12 +79,11 @@ const calcChange = (start, change) => {
         }
     }
     return counter-1;
- }
+ };
 const update = (ci) => {
-    changeAmount.innerHTML = '<p><b>Change in drawer:<b></p>';
+    changeAmount.innerHTML = '<p><b>Change in drawer:</b></p>';
     ci.forEach((item) => {
         changeAmount.innerHTML += `<p id="${item[0]}" class="changes" >${item[0]} $${item[1]}</p>`;
     });
 };
-
 purchaseBtn.addEventListener("click", checkStatus);
